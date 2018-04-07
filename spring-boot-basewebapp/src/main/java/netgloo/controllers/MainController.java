a28de438c8f0b69eb5f0b67d360e94768c1ec327
sunsea <15356365@qq.com>
2018-04-07 15:50:14 +0800

    modified my anno

--- spring-boot-basewebapp/src/main/java/netgloo/controllers/MainController.java
+++ spring-boot-basewebapp/src/main/java/netgloo/controllers/MainController.java
@@ -1,5 +1,5 @@
 package netgloo.controllers;
-//added by sunsea
+//added by sunsea, it's nice job.
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
